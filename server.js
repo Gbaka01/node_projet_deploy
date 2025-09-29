c7e315487f9a5d13c16277d1a5884cbf8b849b05
gbaka01 <gore.goli@gmail.com>
2025-09-29 12:52:36 +0200

    commitgg25

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-  origin: ["https://reseaugbaka.fr"], // 🔒 ton domaine frontend
+  origin: ["https://reseaugbaka.fr"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}))
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors()); // ✅ pour le preflight
 
 
 
