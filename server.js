d1fdd4868e662e28dee845b75a9d281edbf19c0a
gbaka01 <gore.goli@gmail.com>
2025-09-29 10:21:17 +0200

    commitgg19

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://reseaugbaka.fr",  // remplace par ton domaine en https
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 
 
 
